76941c21d5a483fbd0476cad3c9af25f2d21df75
Abhik2005 <142106053+Abhik2005@users.noreply.github.com>
2023-12-21 22:43:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ var circle = document.querySelector(".circle");
 var count = 0;
 circle.addEventListener("click", function () {
   if (count == 0) {
-    console.log("hll");
     circle.style.rotate = "180deg";
     document.querySelector(".circle>i").style.bottom = "45%";
      document.querySelector(".circle>i").style.left = "50%";
